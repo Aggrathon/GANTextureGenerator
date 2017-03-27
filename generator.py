1762140aa56626cc290e1cfa1bb4693fe43dc436
Aggrathon <antonbjo@gmail.com>
2017-03-27 16:15:36 +0300

    Image transformation during training with Pillow

--- generator.py
+++ generator.py
@@ -1,4 +1,5 @@
 import os
+from timeit import default_timer as timer
 import tensorflow as tf
 import numpy as np
 from image import ImageManager
@@ -21,11 +22,12 @@ class Generator():
         setup_folders()
         self.name = name
         self.session = None
+        self.saver = None
         output_size = image_size*image_size*3
         self.layer_data = \
             [int(input_size+i/(hidden_layers+1)*(output_size-input_size)) for i in range(hidden_layers+1)] \
             + [output_size]
-        self.image_manager = ImageManager(INPUT_FOLDER, image_size)
+        self.image_manager = ImageManager(INPUT_FOLDER, OUTPUT_FOLDER, image_size)
         self.batch_size = batch_size
 
         #Handle input
@@ -44,37 +46,34 @@ class Generator():
             self.theta.append(b)
 
         #Handle output
-        self.output = prev_layer# tf.placeholder(tf.uint8, [None, output_size])
+        self.output = prev_layer
 
 
     def random_input(self, n=1):
         return np.random.uniform(-1., 1., size=[n, self.layer_data[0]])
 
 
-    def pretrain(self, epochs = 10):
+    def pretrain(self, epochs = 5000):
+        time = timer()
         y = tf.placeholder(tf.float32, [None,self.layer_data[-1]])
         loss = tf.reduce_mean(tf.nn.l2_loss(tf.pow(self.output-y, 2)))
         train_step = tf.train.AdamOptimizer().minimize(loss, var_list=self.theta)
         self.__create_session__()
-        self.image_manager.read(self.session)
         for i in range(epochs):
             self.session.run(train_step, feed_dict={
                 self.input: self.random_input(self.batch_size),
-                y: tf.to_float(
-                    tf.reshape(
-                        tf.train.shuffle_batch(
-                            [self.image_manager.images], self.batch_size, 256, 128),
-                        [self.batch_size, self.layer_data[-1]]))
-                })
-            print("epoch %d"%i)
+                y: self.image_manager.get_batch(self.batch_size)})
+            if i%20 == 0:
+                print("epoch %d at %.1f"%(i,timer()-time))
+        self.saver.save(self.session, NETWORK_FOLDER)
+        self.__close_session__()
 
 
     def generate(self, save=False):
         self.__create_session__()
         image = self.session.run(self.output, feed_dict={self.input: self.random_input()})
         if save:
-            path = os.path.join(OUTPUT_FOLDER, "generated.png")
-            self.image_manager.write(self.session, tf.cast(image, tf.uint8), path)
+            self.image_manager.write(image)
         return image
 
 
@@ -82,6 +81,17 @@ class Generator():
         if self.session is None:
             self.session = tf.Session()
             self.session.run(tf.global_variables_initializer())
+            self.saver = tf.train.Saver()
+            try:
+                self.saver.restore(self.session, NETWORK_FOLDER)
+            except:
+                print("No already trained network found, creating new")
+
+    def __close_session__(self):
+        if not self.session is None:
+            self.session.close()
+            self.session = None
+
 
 if __name__ == "__main__":
     gen = Generator("test")
--- image.py
+++ image.py
@@ -1,27 +1,90 @@
-import os
-import tensorflow as tf
+import sys, os, string, random, math
+from PIL import Image, ImageEnhance
+import numpy as np
+
 
 class ImageManager():
 
-    def __init__(self, input_folder, output_size):
+    def __init__(self, input_folder, output_folder, image_size, keep_in_memory=True):
+        #Attributes
+        self.keep_in_memory = keep_in_memory
+        self.input_folder = input_folder
+        self.output_folder = output_folder
+        self.image_size = image_size
+        #Config
+        self.max_rotation = 30
+        self.brightness_range = (0.7, 1.1)
+        self.saturation_range = (0.7, 1.)
+        self.contrast_range = (0.8, 1.2)
         #File reading
-        filename_queue = tf.train.string_input_producer(
-            tf.train.match_filenames_once(os.path.join(input_folder, "*.png")))
-        image_reader = tf.WholeFileReader()
-        _, value = image_reader.read(filename_queue)
-        self.images = tf.image.decode_png(value)
-        self.images.set_shape((output_size, output_size, 3))
-        #File writing
-        self.output_file = tf.placeholder(tf.uint8, [output_size, output_size, 3])
-        self.writer = tf.image.encode_png(self.output_file, 5)
-        self.image_size = output_size
+        if keep_in_memory:
+            self.images = [self.read(f) for f in os.listdir(input_folder)]
+            for i in self.images:
+                i.crop((1, 1, 2, 2))
 
-    def read(self, session):
-        tf.train.start_queue_runners(sess=session)
-        return self.images
 
-    def write(self, session, image, path):
-        f = open(path, "wb+")
-        image = tf.reshape(image,(self.image_size, self.image_size, 3))
-        f.write(session.run(self.writer, feed_dict={self.output_file: image}))
-        f.close()
+    def get_batch(self, batch_size=64):
+        if self.keep_in_memory:
+            return np.array([self.get_array(random.choice(self.images)) for _ in range(batch_size)])
+        else:
+            files = os.listdir(self.input_folder)
+            return np.array([self.get_array_from_file(random.choice(files)) for _ in range(batch_size)])
+
+
+    def get_array(self, image):
+        arr = np.asarray(self.get_variation(image))
+        arr.shape = self.image_size*self.image_size*3
+        return arr
+
+    def get_array_from_file(self, file):
+        return self.get_array(self.read(file))
+
+    def get_variation(self, image):
+        #Crop
+        min_dim = min(image.size)
+        scale = max(0.5, math.sqrt(2)*self.image_size/min_dim*1.1)
+        size = int(random.random()*min_dim*(1-scale)+min_dim*scale)
+        pos = (random.randrange(0, image.size[0]-size), random.randrange(0, image.size[1]-size))
+        image = image.crop((pos[0], pos[1], pos[0]+size, pos[1]+size))
+        #Rotate
+        size = image.size[0]/math.sqrt(2)
+        offset = (image.size[0]-size)/2
+        rotation = random.randint(-self.max_rotation, self.max_rotation)
+        image = image.rotate(rotation).crop((offset, offset, offset+size, offset+size))
+        #Transpose
+        if random.random() < 0.5:
+            image.transpose(Image.FLIP_LEFT_RIGHT)
+        #Variation
+        brightness = random.uniform(*self.brightness_range)
+        if np.abs(brightness-1.0) > 0.05:
+            image = ImageEnhance.Brightness(image).enhance(brightness)
+        saturation = random.uniform(*self.saturation_range)
+        if np.abs(saturation-1.0) > 0.05:
+            image = ImageEnhance.Color(image).enhance(saturation)
+        contrast = random.uniform(*self.contrast_range)
+        if np.abs(contrast-1.0) > 0.05:
+            image = ImageEnhance.Contrast(image).enhance(contrast)
+        return image.resize((self.image_size, self.image_size), Image.LANCZOS)
+
+
+    def read(self, file):
+        img = Image.open(os.path.join(self.input_folder, file))
+        return img
+
+    def write(self, image, name=None):
+        image.shape = self.image_size, self.image_size, 3
+        img = Image.fromarray(np.array(image, dtype=np.uint8))
+        if name is None:
+            fullname = "test.png"
+        else:
+            fullname = name+".png"
+        img.save(os.path.join(self.output_folder, fullname), "PNG")
+
+
+if __name__ == "__main__":
+    print("Testing memory requiremens")
+    import generator
+    img = ImageManager(generator.INPUT_FOLDER, generator.OUTPUT_FOLDER, 512)
+    input("Press Enter to continue... (all images loaded)")
+    iml1 = img.get_batch()
+    input("Press Enter to continue... (also one batch)")
